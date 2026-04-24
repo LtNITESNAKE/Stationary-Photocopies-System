2040a36be8eeaad812349ea6f5b0d68e09c05c53
M-awais-sh <sheikhawais413@gmail.com>
2026-04-24 22:11:15 +0500

    added proxy server class and main class code

--- PhotocopyProxy/Program.cs
+++ PhotocopyProxy/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhotocopyProxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Photocopy Proxy Server...");
+
+            // Define backend servers
+            var servers = new List<ServerInfo>
+            {
+                new ServerInfo("127.0.0.1", 5001, 6001, 3), // IP address, port, health check port, weight
+                new ServerInfo("127.0.0.1", 5002, 6002, 2),
+                new ServerInfo("127.0.0.1", 5003, 6003, 1)
+            };
+
+            // Create proxy server
+            ProxyServer proxy = new ProxyServer(7000, servers);
+
+            // Start proxy (this will run continuously)
+            proxy.Start();
+        }
+    }
+}
