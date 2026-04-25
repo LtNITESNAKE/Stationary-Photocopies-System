2d607c6f360a54a2d7778f48fe610d225669c35c
Noor-ul-Huda-129 <nuurulhuda129@gmail.com>
2026-04-25 21:07:16 +0500

    Completed InventoryStock module with concurrency handling

--- Controllers/InventoryStocksController.cs
+++ Controllers/InventoryStocksController.cs
@@ -28,9 +28,67 @@ namespace PhotocopySystem.Controllers
 
         // 2. Edit(int id) - GET
         // TODO: Fetch the specific InventoryStock row and return it to the View.
+         public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var inventoryStock = await _context.InventoryStocks
+                .Include(i => i.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (inventoryStock == null) return NotFound();
+            
+            return View(inventoryStock);
+        }
 
         // 3. Edit(InventoryStock stock) - POST
         // TODO: This is where you catch DbUpdateConcurrencyException! Try saving _context.SaveChanges(). 
         // If it throws the exception, it means a Student bought the item while the Operator was updating it.
+         [HttpPost]
+[ValidateAntiForgeryToken]
+public async Task<IActionResult> Edit(int id, [Bind("Id,ProductId,QuantityAvailable,RowVersion")] InventoryStock stock)
+{
+    if (id != stock.Id) return NotFound();
+
+    if (ModelState.IsValid)
+    {
+        try
+        {
+            _context.Entry(stock).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            var entry = ex.Entries.Single();
+            var databaseValues = await entry.GetDatabaseValuesAsync();
+
+            if (databaseValues == null)
+            {
+                ModelState.AddModelError(string.Empty, 
+                    "Unable to save. The item was deleted by another user.");
+            }
+            else
+            {
+                var dbValues = (InventoryStock)databaseValues.ToObject();
+
+                if (dbValues.QuantityAvailable != stock.QuantityAvailable)
+                {
+                    ModelState.AddModelError("QuantityAvailable", 
+                        $"Current database value: {dbValues.QuantityAvailable}");
+                }
+
+                ModelState.AddModelError(string.Empty,
+                    "The record you attempted to edit was modified by another student. " +
+                    "The current values in the database are displayed below. If you still want to edit, click Save again.");
+
+                stock.RowVersion = dbValues.RowVersion;
+                ModelState.Remove("RowVersion");
+            }
+        }
     }
+
+    return View(stock);
+}
+}
 }
